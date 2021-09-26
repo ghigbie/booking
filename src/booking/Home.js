a78bc2c689f1bdb6fbebc1c00ad33c886bc27a73
ghigbie <gehigbie@gmail.com>
2021-09-26 15:39:56 -0400

    Edit readme

--- src/booking/Home.js
+++ src/booking/Home.js
@@ -1,4 +1,8 @@
+import {useSelector} from 'react-redux';
+
 const Home = () => {
+    const {user} = useSelector((state) => ({...state})); //user is destructured off of state, which is spread onto an object and then returned
+
     return (
         <div className="container-fluid h1 p-5 text-center">
             Home Page
