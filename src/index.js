5a2865ca4a3c4908afb8e2cf00ad7dc621bbee05
ghigbie <gehigbie@gmail.com>
2021-09-27 20:41:38 -0400

    Remove unnecesary export

--- src/index.js
+++ src/index.js
@@ -3,24 +3,10 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import {createStore, combineReducers} from 'redux';
+import {createStore} from 'redux';
 import {Provider} from 'react-redux';
 import {composeWithDevTools} from 'redux-devtools-extension';
-
-const authReducer = (state = {}, action) => {
-  switch(action.type){
-    case "LOGGED_IN_USER":
-      return {...state, ...action.payload};
-    case "LOGOUT":
-      return action.payload;
-    default:
-      return state;
-  }
-}
-
-const rootReducer = combineReducers({
-  user: authReducer
-});
+import rootReducer from './reducers/index';
 
 const store = createStore(rootReducer, composeWithDevTools());
 
